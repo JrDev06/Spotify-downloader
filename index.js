25d9425b8c31514ed43c73a9e7429c3f45139f6a
JrDev06 <jrbusaco72@gmail.com>
2024-07-15 21:27:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fetch from 'node-fetch';
 
 const app = express();
-const PORT = 3000; // Using a fixed port for Render compatibility
+const PORT = 3000; 
 
 app.use(express.json());
 app.use(express.static('public'));
